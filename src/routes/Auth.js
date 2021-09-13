3169c511fb4baa6a1ca98efbf6dbb37a77eb5776
오병현 <obyeonghyeon@obyeonghyeon-ui-MacBookAir.local>
2021-09-13 15:29:35 +0900

    2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,12 @@
+import authService from "fbase";
 import React, { useState } from "react";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
 const Auth = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
 
     const onChange = (event) => {
         const {
@@ -16,16 +19,34 @@ const Auth = () => {
         }
     };
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+
+        try{
+            let data;
+
+            if(newAccount) {
+                // create Account
+                data = await createUserWithEmailAndPassword(authService, email, password);
+            } else {
+                // Log in
+                data = await signInWithEmailAndPassword(authService, email, password);
+            }
+
+            console.log(data);
+
+        }catch(error) {
+            console.log(error);
+        }
+        
     };
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+                <input name="email" type="text" placeholder="email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="password" required value={password} onChange={onChange} />
-                <input type='submit' value="Log In" />
+                <input type='submit' value={newAccount ? "Create Account" : "Log In"} />
             </form>
         </div>
     )
