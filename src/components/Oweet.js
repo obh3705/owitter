430da4d78f52f6998c3719d334bf88e6d4360804
오병현 <obyeonghyeon@obyeonghyeon-ui-MacBookAir.local>
2021-09-18 19:44:02 +0900

    3.5 Update Oweet

--- src/components/Oweet.js
+++ src/components/Oweet.js
@@ -1,7 +1,11 @@
 import { dbService } from "fbase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { useState } from "react";
 
 const Oweet = ({oweetObj, isOwner}) => {
+
+    const [editing, setEditing] = useState(false);
+    const [newOweet, setNewOweet] = useState(oweetObj.text);
 
     const onDeleteClick = async () => {
         const ok = window.confirm("삭제하시겠습니까?");
@@ -17,15 +21,46 @@ const Oweet = ({oweetObj, isOwner}) => {
         
     }
 
+    const toggleEditing = () => {
+        setEditing((prev) => !prev);
+    }
+
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewOweet(value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        // console.log(oweetObj.id, newOweet);
+        await updateDoc(doc(dbService, `oweets/${oweetObj.id}`), {text: newOweet});
+        setEditing(false);
+    };
+
     return (
         <div>
-            <h4>{oweetObj.text}</h4>
-            {isOwner && (
+            {editing ? (
                 <>
-                    <button onClick={onDeleteClick}>Delete Oweet</button>
-                    <button>Edit Oweet</button>
+                    <form onSubmit={onSubmit}>
+                        <input onChange={onChange} value={newOweet} required />
+                        <input type="submit" value="Update Oweet" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
                 </>
+            ) : (
+                    <>
+                        <h4>{oweetObj.text}</h4>
+                        {isOwner && (
+                            <>
+                                <button onClick={onDeleteClick}>Delete Oweet</button>
+                                <button onClick={toggleEditing}>Edit Oweet</button>
+                            </>
+                        )}
+                    </>
             )}
+            
             
         </div>
     )
